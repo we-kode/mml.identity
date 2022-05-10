327ed3111669c07fc0a830a78524bb4a388b1354
Dennis <dstumm@yandex.ru>
2022-05-10 21:17:43 +0200

    Fixed some bugs

---
+++ Identity.Application/IdentityConstants/HttpStatusCodes.cs
@@ -0,0 +1,14 @@
+
+namespace Identity.Application.IdentityConstants
+{
+  /// <summary>
+  /// All available custom http status codes
+  /// </summary>
+  public static class HttpStatusCodes
+  {
+    /// <summary>
+    /// Code when an error in the business logic occurs.
+    /// </summary>
+    public const int BusinessError = 490;
+  }
+}
